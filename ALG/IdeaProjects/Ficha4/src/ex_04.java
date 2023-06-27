d2c7e40f4cc65013c0423b0e3864337cccb99fab
HugoLopes <hugo.lopes.prt_a@msft.cesae.pt>
2023-06-27 12:31:19 +0100

    update

--- ALG/IdeaProjects/Ficha4/src/ex_04.java
+++ ALG/IdeaProjects/Ficha4/src/ex_04.java
@@ -8,13 +8,33 @@ public class ex_04 {
         Scanner input = new Scanner(System.in);
 
         // Declarar Variável
-        int num, aux ;
+        int num;
 
         // Pedir número ao utilizador
         System.out.print("Introduza um número: ");
         num = input.nextInt();
 
         // Verificar se é um número primo
-        
+        if (num>0){
+            if (num == 1 || num != 2 && num % 2 == 0){
+                System.out.println("O número introduzido é não primo!");
+            }else{
+                if (num>=9){
+                    if (num % 3 == 0 || num % 5 == 0 || num % 7 == 0 || num % 9 == 0 ){
+                        System.out.println("O número introduzido é não primo!");
+                    }else{
+                        System.out.println("O número introduzido é primo!");
+                    }
+                }else{
+                    if (num % 3 == 0 && num % 5 == 0 && num % 7 == 0){
+                        System.out.println("O número introduzido é não primo!");
+                    }else{
+                        System.out.println("O número introduzido é primo!");
+                    }
+                }
+            }
+        }else{
+            System.out.println("Introduza um número maior que zero!");
+        }
     }
 }
---
+++ ALG/IdeaProjects/Ficha5/src/ex_01.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ex_01 {
+    public static void main(String[] args) {
+
+        System.out.println("***** Exercício 01 *****");
+
+        // Instanciar Scanner (Linha obrigatória para ler dados da consola)
+        Scanner input = new Scanner(System.in);
+
+        // Declarar array
+        int num[]= new int[10] ;
+
+    }
+}
