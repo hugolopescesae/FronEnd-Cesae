edc0687d2ca54dabd33e11a0f0a7f736073bbd3e
HugoLopes <hugo.lopes.prt_a@msft.cesae.pt>
2023-06-28 17:18:38 +0100

    update

---
+++ EWEB/JSExs/ex_01/JavaScript/script.js
@@ -0,0 +1,24 @@
+//alert("Hello World")
+
+//let myLuckyNumber = 10;
+//let myName = "Hugo";
+
+//alert(myName + " " + myLuckyNumber);
+
+
+//const message=" TASTE THE RAINBOW! ";
+
+//let whisper=message.toLowerCase().trim();
+
+//console.log(whisper);
+
+
+//const word = "skateboard";
+
+//let facialHair = word.slice(5);
+
+//facialHair = facialHair.replace("o","e");
+
+//console.log(facialHair);
+
+
