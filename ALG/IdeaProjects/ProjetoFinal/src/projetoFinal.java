4cae481576653117788df8dd637747fbbc19e355
HugoLopes <hugo.lopes.prt_a@msft.cesae.pt>
2023-07-24 17:15:07 +0100

    update

--- ALG/IdeaProjects/ProjetoFinal/src/projetoFinal.java
+++ ALG/IdeaProjects/ProjetoFinal/src/projetoFinal.java
@@ -1,39 +1,37 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class projetoFinal {
-
     public static String[][] lerFicheiro() throws FileNotFoundException {
         // Ler ficheiro
         File file = new File("files/GameStart.csv");
         Scanner scanner = new Scanner(file);
 
+        // Declarar Variável
+        int numLinhas = 0, numColunas = 0, aux = 0;
         String linha;
-        int counter = 0;
 
-        // Contar quantidade de linhas
+        // Descobrir o numLinhas e numColunas
         while (scanner.hasNextLine()){
             linha = scanner.nextLine();
-            counter++;
+            numLinhas++;
+            numColunas = linha.split(";").length;
         }
+        scanner.close();
 
-        String[][] ficheiro = new String[counter][9];
-
+        // Preencher array
         scanner = new Scanner(file);
+        String[][] ficheiro = new String[numLinhas][numColunas];
 
-        // Contar
         while (scanner.hasNextLine()){
             linha = scanner.nextLine();
-            String[] itensLinha = linha.split(";");
-            for (int i = 0;i < ficheiro.length;i++){
-                for (int j = 0;j < ficheiro[0].length;j++){
-                    ficheiro[i][j] = itensLinha[j];
-                }
+            String[] itensLinhas = linha.split(";");
+            for (int i = 0;i < itensLinhas.length;i++){
+                    ficheiro[aux][i] = itensLinhas[i];
             }
+            aux++;
         }
-
         scanner.close();
 
         return ficheiro;
@@ -43,10 +41,10 @@ public class projetoFinal {
         String[][] matriz = lerFicheiro();
 
         for (int i = 0;i < matriz.length;i++){
-            System.out.print(Arrays.toString(matriz[i]) +"");
-            System.out.println("");
+            for (int j = 0;j < matriz[0].length;j++){
+                System.out.print(matriz[i][j]+" ");
+            }
+            System.out.println();
         }
-
     }
-
 }
